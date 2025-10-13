d6eaffc65671760249821e5df9393de833ce4523
Priyanshu150 <priyanshukumarsingh1502@gmail.com>
2025-10-13 22:26:37 +0530

    Add constructor notes

--- OOPs/Constructor.java
+++ OOPs/Constructor.java
@@ -2,6 +2,30 @@ class Movie{
     public String title;
     public int duration;
 
+    // Default construtor - which is always hidden
+    public Movie(){
+        this.title = null;
+        this.duration = 0;
+    }
+
+    // Custom Default constructor - used for hard cording mostly 
+    public Movie(){
+        this.title = "Untitled";
+        this.duration = 0;
+    }
+
+    // Paramerized constructor 
+    public Movie(String title, int duration){
+        this.title = title;
+        this.duration = duration;
+    }
+
+    // Copy constructor 
+    public Movie(Movie other){
+        this.title = other.title;
+        this.duration = other.duration;
+    }
+
     public void displayDetials(){
         System.out.println("Title: " + title + ", Duration: " + duration + " mins");
     }
@@ -10,6 +34,32 @@ class Movie{
 public Class Main {
     public static void main(String[] args){
         Movie movie = new Movie();
+        Movie movie2 = new Movie('Harry Potter', 145);
+        Movie copyMovie = new Movie(movie2);
+
         movie.displayDetials();
+    }
+}
+
+class Singleton{
+    public static Singleton intance;
+
+    // private contructor 
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+        if(instance == null)        
+            intance = new Singleton();
+        
+        return intance;
+    }
+}
+
+pulic class Main{
+    public static void main(String[] args){
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.err.println(s1 == s2);
     }
 }
